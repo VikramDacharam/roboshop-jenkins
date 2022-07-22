3c5159b19f46edfb9270e424f8b39608a2cbb1e2
Vikram Dacharam <vikramdacharam95@gmail.com>
2022-07-22 22:58:38 +0530

    aa

--- vars/ansible.groovy
+++ vars/ansible.groovy
@@ -1,55 +1,64 @@
-def call (){
+def call() {
     pipeline {
         agent any
 
-        parameters {
-            string(name: 'ENV', defaultValue: '', description: 'which environment?')
-            string(name: 'COMPONENT', defaultValue: '', description: 'which Component?')
+        options {
+            ansiColor('xterm')
         }
+
+        parameters {
+            string(name: 'ENV', defaultValue: '', description: 'Which Environment?')
+            string(name: 'COMPONENT', defaultValue: '', description: 'Which Component?')
+        }
+
         environment {
             SSH = credentials('SSH')
         }
-        stages{
-            stage('Checkout code - DEV'){
+
+        stages {
+
+            stage('CheckOut Code - DEV') {
                 when {
-                    expression{
+                    expression {
                         ENV == "dev"
                     }
                 }
-                steps{
-                    dir('ANSIBLE'){
-                        git branch: 'dev', url: 'https://github.com/VikramDacharam/roboshop-jenkins.git'
+                steps {
+                    dir('ANSIBLE') {
+                        git branch: 'dev', url: 'https://github.com/raghudevopsb64/roboshop-ansible.git'
                     }
-
                 }
             }
-            stage('Checkout code - PROD'){
+
+            stage('CheckOut Code - PROD') {
                 when {
-                    expression{
+                    expression {
                         ENV == "prod"
                     }
                 }
-                steps{
-                    dir('ANSIBLE'){
-                        git branch: 'main', url: 'https://github.com/VikramDacharam/roboshop-jenkins.git'
+                steps {
+                    dir('ANSIBLE') {
+                        git branch: 'main', url: 'https://github.com/raghudevopsb64/roboshop-ansible.git'
                     }
                 }
             }
-        }
-        stages{
-            stage("create instance"){
-                steps{
+
+            stage('Create Instance') {
+                steps {
                     dir('ANSIBLE') {
                         sh 'bash create-ec2-with-env.sh ${COMPONENT} ${ENV}'
                     }
                 }
             }
-            stage(' Run Ansible '){
+
+            stage('Run Ansible Playbook') {
                 steps {
                     dir('ANSIBLE') {
-                        sh 'ansible-playbook -i inv roboshop.yml -e HOST=${COMPONENT} -e ROLE_NAME=${COMPONENT} -e ENV=${ENV} -e ansible_user=${SSH_USR} -e ansible_password=${SSH_PSW}'
-                    }               }
+                        sh 'ansible-playbook -i inv roboshop.yml -e ENV=${ENV} -e ansible_user=${SSH_USR} -e ansible_password=${SSH_PSW} -e HOST=${COMPONENT} -e ROLE_NAME=${COMPONENT}'
+                    }
+                }
             }
+
         }
 
     }
