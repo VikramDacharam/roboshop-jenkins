3bd8b7195e8d31e33b7bd24909b3df5d56e78d2a
Vikram Dacharam <vikramdacharam95@gmail.com>
2022-08-07 13:15:13 +0530

    aa

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -2,6 +2,10 @@ def call() {
     pipeline {
         agent any
 
+        options {
+            ansiColor('xterm')
+        }
+
         parameters {
             string(name: 'ENV', defaultValue: '', description: 'Which Environment?')
         }
@@ -11,20 +15,30 @@ def call() {
         }
 
         stages {
+
             stage('Terraform INIT') {
                 steps {
                     sh '''
                       terraform init -backend-config=env-${ENV}/backend.tfvars
                     '''
                 }
-            stage('Terraform apply'){
+            }
+
+            stage('Terraform Apply') {
                 steps {
                     sh '''
-                      terraform init -var-file=env-${ENV}/{ENV}.tfvars
+                      terraform apply -auto-approve -var-file=env-${ENV}/${ENV}.tfvars
                     '''
                 }
             }
+
+        }
+
+        post {
+            always {
+                cleanWs()
             }
         }
+
     }
 }
